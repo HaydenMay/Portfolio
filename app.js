f7c2ab770c9e6b88e89a1d372e09f7d73082cf3d
Hayden May (OSV) <hayden.may.osv@fedex.com>
2023-06-30 07:15:03 -0700

    updates to add second page and themes

--- app.js
+++ app.js
@@ -36,4 +36,53 @@ function pageTransitions(){
     })
 }
 
+   const themeBtn = document.querySelector('.theme-btn');
+   let i = 0
+   themeBtn.addEventListener('click', () => {
+       let element = document.body
+       if(i == 0){
+        element.classList.remove('green')
+        element.classList.toggle('purple')
+       }
+       if(i == 1){
+        element.classList.remove('purple')
+        element.classList.add('pink')
+       }
+       if(i == 2){
+        element.classList.remove('pink')
+        element.classList.add('blue')
+       }
+       if(i == 3){
+        element.classList.remove('blue')
+        element.classList.add('yellow')
+       }
+       if(i == 4){
+        element.classList.remove('yellow')
+        element.classList.add('grey')
+       }
+       if(i== 5){
+        element.classList.remove('grey')
+        element.classList.add('green')
+
+        i = -1
+       }
+       i++
+   })
+
+   const darkBtn = document.querySelector('.dark-btn');
+   const lightBtn = document.querySelector('.light-btn');
+   lightBtn.addEventListener('click', () => {
+       let element = document.body
+        element.classList.add('light-mode')
+        darkBtn.classList.remove('hidden')
+        lightBtn.classList.add('hidden')
+   })
+
+   darkBtn.addEventListener('click', () => {
+       let element = document.body
+        element.classList.remove('light-mode')
+        darkBtn.classList.add('hidden')
+        lightBtn.classList.remove('hidden')
+   })
+
 pageTransitions();
